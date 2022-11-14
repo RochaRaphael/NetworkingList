6b80ead4769ed667c02bf607d2f7895dcc948bb0
Raphael Rocha <rocharaphael0911@outlook.com>
2022-11-14 16:06:57 -0300

    Adding propeties to SkillMap

---
+++ Data/Mapping/SkillsMap.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NetworkingList.Models;
+
+namespace NetworkingList.Data.Mappings
+{
+    public class SkillMap : IEntityTypeConfiguration<Skill>
+    {
+        public void Configure(EntityTypeBuilder<Skill> builder)
+        {
+            builder.ToTable("Skill");
+
+            //Primary key
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd()
+            .UseIdentityColumn();
+
+            builder.Property(x => x.SkillName)
+            .IsRequired() //Not null
+            .HasColumnName("Name")
+            .HasColumnType("NVARCHAR")
+            .HasMaxLength(80);
+        }
+    }
+}
