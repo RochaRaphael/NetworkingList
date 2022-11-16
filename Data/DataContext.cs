cd81070a1db6dd47457c3804d0d29411fb0aa8dc
Raphael Rocha <rocharaphael0911@outlook.com>
2022-11-16 16:55:05 -0300

    fixing connection string

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -11,7 +11,7 @@ namespace NetworkingList.DataContext
 
         //Database Connections
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlServer("Server=localhost,1433;Database=NetworkingFluentMap;User ID=sa;Password=1q2w3e4r@#$");
+            => options.UseSqlServer("Server=localhost,1433;Database=NetworkingFluentMap;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;");
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
