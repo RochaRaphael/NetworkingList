a1a3cde4a44a1b17ca31c1523c1699216fc547f4
Raphael Rocha <rocharaphael0911@outlook.com>
2022-11-17 13:19:39 -0300

    Create a view structe in Program.cs

--- Program.cs
+++ Program.cs
@@ -9,6 +9,45 @@ namespace NetworkingList
         static void Main(string[] args)
         {
 
+            void Menu()
+            {
+                Console.WriteLine("CHOSE AN OPTION - PUT A NUMBER");
+                Console.WriteLine();
+                Console.WriteLine("1- Insert contact");
+                Console.WriteLine("2- Delete contact");
+                Console.WriteLine("3- Update contact");
+                Console.WriteLine("4- Search");
+                Console.WriteLine("5- Close the program");
+            }
+
+
+            int option = 1;
+            while (option < 5 && option > 0)
+            {
+                Console.Clear();
+                Menu();
+                option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        break;
+
+                    case 2:
+                        break;
+
+                    case 3:
+                        break;
+
+                    case 4:
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("BYE, SEE YOU SOON!");
+                        break;
+                }
+            }
         }
     }
 }
